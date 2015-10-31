c15eb0d4860c29e311fd1848b85299d906f3ee4b
Joern Bernhardt <jb@campudus.com>
2015-10-31 21:40:42 +0100

    Fix height calculations
    Signed-off-by: Joern Bernhardt <jb@campudus.com>

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,19 @@
-console.log('hello.');
+var description = document.getElementById('description-info');
+var agenda = document.getElementById('agenda-info');
+
+function resize() {
+  var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+
+  if (w >= 1000) {
+    var maxHeight = Math.max(description.offsetHeight, agenda.offsetHeight);
+
+    description.style.height = maxHeight + 'px';
+    agenda.style.height = maxHeight + 'px';
+  } else {
+    description.style.height = 'auto';
+    agenda.style.height = 'auto';
+  }
+}
+
+window.onresize = resize;
+resize();
