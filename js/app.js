c4f7eb740ba762645d55ba7af8a390daa377a521
Joern Bernhardt <jb@campudus.com>
2015-11-05 00:57:05 +0100

    Update 2015-11-04T23:57:03.175Z

--- js/app.js
+++ js/app.js
@@ -4,9 +4,13 @@ var agenda = document.getElementById('agenda-info');
 
 function resize() {
   var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+  var maxHeight;
 
-  if (w >= 1000) {
-    var maxHeight = Math.max(description.offsetHeight, agenda.offsetHeight);
+  if (w > 1000) {
+    description.style.height = 'auto';
+    agenda.style.height = 'auto';
+
+    maxHeight = Math.max(description.offsetHeight, agenda.offsetHeight);
 
     description.style.height = maxHeight + 'px';
     agenda.style.height = maxHeight + 'px';
